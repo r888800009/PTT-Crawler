00f02b479e0f2b303d91f8dadd360a6e9909329f
Lin, Yong Xiang <r888800009@gmail.com>
2024-10-16 01:20:08 +0800

    WIP: add PttWebCrawler parser

--- Crawler.py
+++ Crawler.py
@@ -1,5 +1,6 @@
 # -*- coding: UTF-8 -*-
 
+import asyncio
 import json
 import requests
 import time
@@ -7,10 +8,14 @@ import time
 from bs4 import BeautifulSoup
 from bs4.element import NavigableString
 
-def main():
+from pyppeteer import launch
 
-    crawler = PttCrawler()
-    crawler.crawl(board="Gossiping", start=10001, end=11000)
+def main():
+    # 我們不 main 直接做 test
+    test_parse_article()
+
+    #crawler = PttCrawler()
+    #crawler.crawl(board="Gossiping", start=10001, end=11000)
 
     #res = crawler.parse_article("https://www.ptt.cc/bbs/Gossiping/M.1119928928.A.78A.html")
     #crawler.output("test", res)
@@ -109,37 +114,38 @@ class PttCrawler:
         novote = 0
         response_list = []
 
-        push_list = soup.select(".push")
+        push_list = self.get_push_list(soup)
         for response_struct in push_list:
 
             #跳脫「檔案過大！部分文章無法顯示」的 push class
-            if "warning-box" not in response_struct['class']:
+            if self.push_list_skip_condition(response_struct):
+                continue
 
-                response_dic = {}
+            response_dic = {}
+            
+            # 根據不同的mode去採集response
+            if mode == 'all':
+                self.parse_response(response_struct, response_dic)
+                response_list.append(response_dic)
                 
-                # 根據不同的mode去採集response
-                if mode == 'all':
-                    self.parse_response(response_struct, response_dic)
+                if response_dic["Vote"] == u"推":
+                    upvote += 1
+                elif response_dic["Vote"] == u"噓":
+                    downvote += 1
+                else:
+                    novote += 1
+            else:
+                response_dic = self.parse_response(response_struct, response_dic)
+
+                if response_dic["Vote"] == mode:
                     response_list.append(response_dic)
                     
-                    if response_dic["Vote"] == u"推":
+                    if mode == u"推":
                         upvote += 1
-                    elif response_dic["Vote"] == u"噓":
+                    elif mode == u"噓":
                         downvote += 1
                     else:
                         novote += 1
-                else:
-                    response_dic = self.parse_response(response_struct, response_dic)
-
-                    if response_dic["Vote"] == mode:
-                        response_list.append(response_dic)
-                        
-                        if mode == u"推":
-                            upvote += 1
-                        elif mode == u"噓":
-                            downvote += 1
-                        else:
-                            novote += 1
 
         article["Responses"] = response_list
         article["UpVote"] = upvote
@@ -148,8 +154,16 @@ class PttCrawler:
 
         return article
 
+    def push_list_skip_condition(self, response_struct):
+        return "warning-box" in response_struct['class']
+
+    def get_push_list(self, soup):
+        return soup.select(".push")
+
     def get_content(self, soup, article):
         content = ""
+        # BUG: 這個 Parsing 方式只會得到其中一行，因為有時候會有多行，就不會得到正確的內文
+        # NOTE: 如果要進行修改，也務必檢查 PttWebCrawler.get_content 的內容
         for tag in soup.select("#main-content")[0]:
             if type(tag) is NavigableString and tag !='\n':
                 content += tag
@@ -163,7 +177,7 @@ class PttCrawler:
     def parse_response(self, response_struct, response_dic):
         """response_dic 回傳內容
         """
-        response_dic["Content"] = response_struct.select(".push-content")[0].contents[0][1:]
+        response_dic["Content"] = response_struct.select(".push-content")[0].contents[0][2:]
         response_dic["Vote"]  = response_struct.select(".push-tag")[0].contents[0][0]
         response_dic["User"]  = response_struct.select(".push-userid")[0].contents[0]
 
@@ -203,6 +217,100 @@ class PttCrawler:
             
             start += 1
 
+class PttWebCrawler(PttCrawler):
+
+    def parse_article(self, url, mode):
+        text = self.open_url_sync(url)
+        return self.parse_article_no_request(text, mode)
+    
+    def open_url_sync(self, url):
+        return asyncio.run(self.open_url(url))
+
+    async def open_url(self, url):
+        browser = await launch()
+        page = await browser.newPage()
+        await page.goto(url)
+        
+        # 如果有 button 就點擊
+        button = await page.xpath('//div[contains(text(), "載入全部")]/parent::button')
+        if button:
+            print('有 button 就點擊')
+            # TODO: 這目前點擊後不會有效果，需要進一步確認
+            await button[0].click()
+
+        # 回傳 html
+        html_doc = await page.content()
+        await browser.close()
+        return html_doc
+
+    def get_content(self, soup, article):
+        # BUG: 這個為了維持介面一致，我們採用 PttCrawler.get_content 的方式，也因此每次只會爬取一行
+        content = ""
+        for tag in soup.find("div", itemprop="articleBody"):
+            # check class is "" and tag is span
+            if len(tag.get("class")) == 0 and tag.name == "span" and tag !='\n':
+                content += tag.text
+                break
+        article["Content"] = content
+
+    def get_title_and_author(self, soup, article):
+        # h1 title mt-2
+        article["Title"] = soup.find_all("h1", class_="title")[0].text.strip()
+        article["Author"] = soup.find("span", itemprop="name").text
+
+    def get_push_list(self, soup):
+        print(soup.find_all("div", itemprop="comment"))
+        return soup.find_all("div", itemprop="comment")
+
+    def parse_response(self, response_struct, response_dic):
+        """response_dic 回傳內容
+        """
+        response_dic["Content"] = response_struct.find("div", itemprop="text").text
+        response_dic["Vote"]  = response_struct.find("div", class_="e7-left").text
+        response_dic["User"]  = response_struct.find("span", itemprop="name").text
+
+    def push_list_skip_condition(self, response_struct):
+        # don't care
+        return False
+
+def test_parse_article():
+    article_list = [
+        'Gossiping/M.1694692614.A.53F',
+        'Linux/M.1117722226.A.5BB',
+        'Gossiping/M.1528987377.A.B29',
+        'Gossiping/M.1694692327.A.BC3',
+    ]
+    
+    test_mode = [
+        'all', 
+        #'up',
+        #'down',
+        #'normal',
+    ]
+
+    for article in article_list:
+        for mode in test_mode:
+            crawler = PttCrawler()
+            url = f"https://www.ptt.cc/bbs/{article}.html"
+            print(url)
+            ptt_result = crawler.parse_article(url, mode=mode)
+
+            crawler = PttWebCrawler()
+            url = f"https://www.pttweb.cc/bbs/{article}"
+            print(url)
+            pttweb_result = crawler.parse_article(url, mode=mode)
+            for key in ptt_result:
+                debug_print(ptt_result, pttweb_result, key)
+                assert ptt_result[key] == pttweb_result[key], f"article: {article}, mode: {mode}, key: {key}"
+
+def debug_print(ptt_result, pttweb_result, key):
+    if key == "Content":
+        print("=== content ptt ===")
+        print(ptt_result[key])
+        print("=== content pttweb ===")
+        print(pttweb_result[key])
+    else:
+        print(key, ptt_result[key], pttweb_result[key])
 
 if __name__ == '__main__':
     main()
